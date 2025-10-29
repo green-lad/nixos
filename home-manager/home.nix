49bb462fb1d56f6c564b34572e76d4ebfe998996
Markus Schoetz <markus.schoetz@fau.de>
2025-10-29 03:02:42 +0100

    fix oathtool not getting found

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -65,63 +65,51 @@
       HOST = "${hostname}";
       TERMINAL = "wezterm";
     };
-    packages =
-      with pkgs;
-      let
-        oathtool = (
-          python3.withPackages (
-            p:
-            (with p; [
-              oathtool
-            ])
-          )
-        );
-      in
-      [
-        blender
-        brightnessctl
-        cura-appimage
-        delta
-        gimp
-        gnumake
-        go
-        graphviz
-        htop
-        inkscape
-        inputs.additional-fonts.packages.${system}.astetica
-        inputs.additional-fonts.packages.${system}.leafery
-        jq
-        kdePackages.okular
-        kicad
-        killall
-        lazygit
-        libreoffice
-        libsForQt5.qt5.qtwayland
-        lightburn
-        mplayer
-        mpv
-        nautilus
-        ngspice
-        oathtool
-        obs-cmd
-        obs-studio
-        openscad
-        pastel
-        pulseaudio
-        pulsemixer
-        qt6.qtwayland
-        scooter
-        songrec
-        speedtest-cli
-        supercollider
-        swayimg
-        termdown
-        unzip
-        wev
-        wl-clipboard
-        yt-dlp
-        zathura
-      ];
+    packages = with pkgs; [
+      (python3.withPackages (p: (with p; [ oathtool ])))
+      blender
+      brightnessctl
+      cura-appimage
+      delta
+      gimp
+      gnumake
+      go
+      graphviz
+      htop
+      inkscape
+      inputs.additional-fonts.packages.${system}.astetica
+      inputs.additional-fonts.packages.${system}.leafery
+      jq
+      kdePackages.okular
+      kicad
+      killall
+      lazygit
+      libreoffice
+      libsForQt5.qt5.qtwayland
+      lightburn
+      mplayer
+      mpv
+      nautilus
+      ngspice
+      obs-cmd
+      obs-studio
+      openscad
+      pastel
+      pulseaudio
+      pulsemixer
+      qt6.qtwayland
+      scooter
+      songrec
+      speedtest-cli
+      supercollider
+      swayimg
+      termdown
+      unzip
+      wev
+      wl-clipboard
+      yt-dlp
+      zathura
+    ];
   };
 
   fonts.fontconfig.enable = true;
