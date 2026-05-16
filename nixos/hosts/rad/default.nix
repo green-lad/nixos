996d44c7963814742ae1071b629903f6a0cc3d7b
Markus Schoetz <markus.schoetz@fau.de>
2026-05-17 00:58:28 +0200

    get gpu working

--- nixos/hosts/rad/default.nix
+++ nixos/hosts/rad/default.nix
@@ -1,3 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  boot.kernelModules = [ "uinput" "amdgpu" ];
+
+  hardware = {
+    amdgpu = {
+      initrd.enable = true;
+      opencl.enable = true;
+    };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        mesa
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+    };
+  };
 }
